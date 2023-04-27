a65349deb1ef9f0d95ba4c507b25260697ebfd43
Junaid Anjum <52190067+junaidanjum@users.noreply.github.com>
2023-04-27 23:28:19 +0530

    config provided updated with font and color

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,18 @@ import '@/styles/globals.css'
 import { ConfigProvider } from 'antd'
 import type { AppProps } from 'next/app'
 
+// import { Lato } from 'next/font/google'
+// const font = Lato({ weight: ['100', '300', '400', '700', '900'], subsets: ['latin'] })
+// const PRIMARY_COLOR = 'red'
+
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<ConfigProvider
 		// theme={{
 		// 	token: {
-		// 		fontFamily: 'Noto Sans',
+		//		fontFamily: font.style.fontFamily,
 		// 		borderRadius: 0,
-		// 		colorPrimary: 'blue'
+		// 		colorPrimary: PRIMARY_COLOR
 		// 	}
 		// }}
 		>
