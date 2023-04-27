ded002c5a3c669eb107764ab12b101fb4867b60a
Junaid Anjum <52190067+junaidanjum@users.noreply.github.com>
2023-04-27 22:00:45 +0530

    added antdesign, tabler icons, axios, uuid, moment

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
 import '@/styles/globals.css'
+import { ConfigProvider } from 'antd'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<ConfigProvider
+		// theme={{
+		// 	token: {
+		// 		fontFamily: 'Noto Sans',
+		// 		borderRadius: 0,
+		// 		colorPrimary: 'blue'
+		// 	}
+		// }}
+		>
+			<Component {...pageProps} />
+		</ConfigProvider>
+	)
 }
